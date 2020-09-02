82bafbf27dc3540b5eaedd1525106654ee6bceca
redspit <49604325+redspit@users.noreply.github.com>
2020-09-02 14:10:01 +0300

    First Step1

--- app/src/main/java/com/test/learn_words/MainActivity.java
+++ app/src/main/java/com/test/learn_words/MainActivity.java
@@ -1,8 +1,11 @@
 package com.test.learn_words;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Message;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +13,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(this, "onCreate Phase", Toast.LENGTH_LONG).show();
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(this, "onResume Phase", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(this, "onPause Phase", Toast.LENGTH_LONG).show();
     }
 
 }
