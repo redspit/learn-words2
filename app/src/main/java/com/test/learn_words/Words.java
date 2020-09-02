8e13bcf39d5d5a64d7beec2a836282883351877e
redspit <49604325+redspit@users.noreply.github.com>
2020-09-02 12:40:55 +0300

    First Step

---
+++ app/src/main/java/com/test/learn_words/Words.java
@@ -0,0 +1,16 @@
+package com.test.learn_words;
+
+public class Words {
+    String target_word;
+    String english_word;
+    String language;
+    String hiragana = "";
+    String kanji = "";
+    String katakana = "";
+    String hanzi = "";
+
+    public Words() {
+
+    }
+
+}
